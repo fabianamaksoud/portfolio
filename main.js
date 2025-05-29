c2178330895911093a1fd79442d0967b49ef06e1
fabianamaksoud <maksoud.fabi@gmail.com>
2025-05-29 08:28:40 -0300

    setas animação

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 window.addEventListener("load", (event) => {
-
-    const familyHeaders = document.getElementsByClassName('project-family-header')
-        for (let i = 0; i < familyHeaders.length; i++) {
-            familyHeaders[i].addEventListener('click', (()=>{
+    const familyHeaders = document.getElementsByClassName('project-family-header');
+    const displayedStatuses = []; // false = closed state
+    for (let i = 0; i < familyHeaders.length; i++) {
+        displayedStatuses.push(false)
+        familyHeaders[i].addEventListener('click', (()=>{
             let work = document.getElementsByClassName('work')[i];
             work.style.display = work.style.display === 'none' ? '' : 'none';
+            let arrow = document.getElementsByClassName('project-family-arrow')[i];
+            displayedStatuses[i] = !displayedStatuses[i];
+            arrow.style.transform =  `rotate(${displayedStatuses[i] ? '180' : '0'}deg)`;
         }));
     }
 });
